8e1e66abb874de6881d53570df9d712956195621
Jananayani <abjmaheshika@gmail.com>
2023-03-29 16:42:47 +0530

    Updated three files

--- script.js
+++ script.js
@@ -3,31 +3,52 @@ if(el){
   el.addEventListener('click', showUsername);
 }
 
-// document.getElementById(btn).addEventListener('click',showUsername);
-
 function showUsername(){
     console.log('Calling');
 
     let username = document.getElementById('username').value
 
-    let url = 'https://api.github.com/users?q=$(username)' + username
-    fetch(url).then(res=>res.json()).then(data=>{
+    let url = `https://api.github.com/users/${username}?q=Q`
+    let token = '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'; 
+    fetch(url, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then(res => res.json())
+    .then(data => {
         if(data.message){
             document.getElementById('res').innerHTML = `
             <h3>Not Found</h3>
             `
-        }else{
-            // console.log(data)
+        } else {
             document.getElementById('res').innerHTML = `
-                <img src = "${data.avatar_url}">
-                <p>${data.name}(${data.login})</p>
-                <p>${data.bio}</p>
-                <p>${data.repos_url}</p>
+            <div class="border">
+                <div>
+                    <img class="image" src="${data.avatar_url}" />
+                </div>
+                <div class="details">
+                    <h2 style="color:black">${data.name}</h2>
+                    <p style="color:black">${data.login}</p>
+                    <p style="color:black">${data.bio}</p>
+                    <ul class="info">
+                        <li>${data.followers}<strong> Followers &nbsp</strong></li></br>
+                        <li>${data.following}<strong> Following &nbsp</strong></li>
+                        <li>${data.public_repos}<strong> Public Repositories</strong></li>
+                    </ul>
+                    <div id="repos"></div>
+                </div>
+        </div>
+               
+                   
+                        
+                    
+                
+            
             `
         }
-        
-    }).catch(e=>{
+    })
+    .catch(e => {
         console.log(e);
     })
-    
 }
