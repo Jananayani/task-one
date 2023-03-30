0835b6a489c20e7aaed8ffa4d45746105848653f
Jananayani <abjmaheshika@gmail.com>
2023-03-30 12:57:38 +0530

    Add some styles

--- script.js
+++ script.js
@@ -1,54 +1,47 @@
-var el = document.getElementById('btn');
-if(el){
-  el.addEventListener('click', showUsername);
-}
+var el = document.getElementById("btn");
+// if(el){
+el.addEventListener("click", showUsername);
+// }
 
-function showUsername(){
-    console.log('Calling');
+async function showUsername() {
+  console.log("Calling");
 
-    let username = document.getElementById('username').value
+  let username = document.getElementById("username").value;
 
-    let url = `https://api.github.com/users/${username}?q=Q`
-    let token = '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'; 
-    fetch(url, {
-        headers: {
-            'Authorization': `Bearer ${token}`
-        }
-    })
-    .then(res => res.json())
-    .then(data => {
-        if(data.message){
-            document.getElementById('res').innerHTML = `
+  let url = `https://api.github.com/users/${username}?q=Q`;
+  let token = "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";
+  try {
+    const res = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const data = await res.json();
+    if (data.message) {
+      document.getElementById("result").innerHTML = `
             <h3>Not Found</h3>
-            `
-        } else {
-            document.getElementById('res').innerHTML = `
-            <div class="border">
-                <div>
-                    <img class="image" src="${data.avatar_url}" />
-                </div>
-                <div class="details">
-                    <h2 style="color:black">${data.name}</h2>
-                    <p style="color:black">${data.login}</p>
-                    <p style="color:black">${data.bio}</p>
-                    <ul class="info">
-                        <li>${data.followers}<strong> Followers &nbsp</strong></li></br>
-                        <li>${data.following}<strong> Following &nbsp</strong></li>
-                        <li>${data.public_repos}<strong> Public Repositories</strong></li>
-                    </ul>
-                    <div id="repos"></div>
-                </div>
-        </div>
-               
-                   
-                        
-                    
-                
-            
-            `
-        }
-    })
-    .catch(e => {
-        console.log(e);
-    })
+            `;
+    } else {
+      document.getElementById("result").innerHTML = `
+      <div class="card">
+              <div>
+                <img class="image" src="${data.avatar_url}" />
+              </div>
+              <div class="details">
+                <h2>${data.name}</h2>
+                <p>${data.login}</p>
+                <p>${data.bio}</p>
+                <ul>
+                  <li>${data.followers} Followers &nbsp</li>
+                  <li>${data.following} Following &nbsp</li>
+                  <li>${data.public_repos} Public Repositories</li>
+                </ul>
+              </div>
+            </div>
+       
+      `;
+    }
+  } catch (e) {
+    console.log(e);
+  }
 }
