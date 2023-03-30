460c8a2157adfc3e0876a637f9c13e30f8d4a3ee
Jananayani <abjmaheshika@gmail.com>
2023-03-30 13:43:13 +0530

    Update js file

--- js/script.js
+++ js/script.js
@@ -1,47 +1,44 @@
-var el = document.getElementById("btn");
-// if(el){
-el.addEventListener("click", showUsername);
-// }
+var el = document.getElementById('btn');
+if(el){
+  el.addEventListener('click', showUsername);
+}
 
-async function showUsername() {
-  console.log("Calling");
+// document.getElementById(btn).addEventListener('click',showUsername);
 
-  let username = document.getElementById("username").value;
+function showUsername(){
+    console.log('Calling');
 
-  let url = `https://api.github.com/users/${username}?q=Q`;
-  let token = "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";
-  try {
-    const res = await fetch(url, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    const data = await res.json();
-    if (data.message) {
-      document.getElementById("result").innerHTML = `
+    let username = document.getElementById('username').value
+
+    let url = `https://api.github.com/users/${username}?q=Q`
+    fetch(url).then(res=>res.json()).then(data=>{
+        if(data.message){
+            document.getElementById('result').innerHTML = `
             <h3>Not Found</h3>
-            `;
-    } else {
-      document.getElementById("result").innerHTML = `
-      <div class="card">
-              <div>
-                <img class="image" src="${data.avatar_url}" />
-              </div>
-              <div class="details">
-                <h2>${data.name}</h2>
-                <p>${data.login}</p>
-                <p>${data.bio}</p>
-                <ul>
-                  <li>${data.followers} Followers &nbsp</li>
-                  <li>${data.following} Following &nbsp</li>
-                  <li>${data.public_repos} Public Repositories</li>
-                </ul>
-              </div>
+            `
+        }else{
+            // console.log(data)
+            document.getElementById('result').innerHTML = `
+            <div class="card">
+            <div>
+              <img class="image" src="${data.avatar_url}" />
             </div>
-       
-      `;
-    }
-  } catch (e) {
-    console.log(e);
-  }
+            <div class="details">
+              <h2>${data.name}</h2>
+              <p>${data.login}</p>
+              <p>${data.bio}</p>
+              <ul>
+                <li>${data.followers} Followers &nbsp</li>
+                <li>${data.following} Following &nbsp</li>
+                <li>${data.public_repos} Public Repositories</li>
+              </ul>
+            </div>
+          </div>
+            `
+        }
+        
+    }).catch(e=>{
+        console.log(e);
+    })
+    
 }
